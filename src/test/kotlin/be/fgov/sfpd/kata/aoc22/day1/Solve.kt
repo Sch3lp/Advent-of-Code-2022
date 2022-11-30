458702d944c2681b49b1eeef01c1a1b29dddef30
Tim Schraepen <tim.schraepen@sfpd.fgov.be>
2022-11-30 11:17:19 +0100

    Delete solution of 2021 day1

--- src/test/kotlin/be/fgov/sfpd/kata/aoc22/day1/Solve.kt
+++ src/test/kotlin/be/fgov/sfpd/kata/aoc22/day1/Solve.kt
@@ -9,22 +9,22 @@ class Solve {
     @Test
     fun `example input part 1`() {
         val input = readFile("day1/exampleInput.txt")
-        assertThat(solve1(input)).isEqualTo(7)
+        assertThat(solve1(input)).isEqualTo(42)
     }
 
     @Test
     fun `actual input part 1`() {
         val input = readFile("day1/input.txt")
-        assertThat(solve1(input)).isEqualTo(7)
+        assertThat(solve1(input)).isEqualTo(42)
     }
 
     @Test
     fun `actual input part 2`() {
         val input = readFile("day1/input.txt")
-        assertThat(solve2(input)).isEqualTo(7)
+        assertThat(solve2(input)).isEqualTo(42)
     }
 
-    private fun solve1(input: String) : Int = solution1(input)
+    private fun solve1(input: String) : Int = TODO()
     private fun solve2(input: String) : Int = TODO()
 
 }
