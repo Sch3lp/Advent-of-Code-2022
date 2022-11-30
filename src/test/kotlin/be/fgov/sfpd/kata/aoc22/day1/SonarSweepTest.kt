097fab54fabde51d78d1bcddd3171403dc0dab95
Jonas Van Hoeyweghen <jonas.VANHOEYWEGHEN@sfpd.fgov.be>
2022-11-30 10:38:05 +0100

    implemented counting the amount of depth increases

--- src/test/kotlin/be/fgov/sfpd/kata/aoc22/day1/SonarSweepTest.kt
+++ src/test/kotlin/be/fgov/sfpd/kata/aoc22/day1/SonarSweepTest.kt
@@ -1,6 +1,6 @@
 import DepthChange.DECREASED
 import DepthChange.INCREASED
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class SonarSweepTest {
@@ -8,14 +8,23 @@ class SonarSweepTest {
     internal fun `given a sweep report, we measure the depth changes`() {
         val report = listOf(1, 2, 3)
         val result = measure(report)
-        Assertions.assertThat(result).containsExactly(INCREASED, INCREASED)
+        assertThat(result).containsExactly(INCREASED, INCREASED)
     }
 
-    private fun measure(report: List<Int>): List<DepthChange> {
-        val depthMeasurements: List<Pair<Int, Int>> = report.zipWithNext()
-        return depthMeasurements.map { (previous, current) -> if (current > previous) INCREASED else DECREASED }
+    @Test
+    internal fun `given a sweep report, we can count the amount of increases`() {
+        val report = listOf(1, 2, 3)
+
+        val result = measure(report).countNumberOfDepthIncreases()
+
+        assertThat(result).isEqualTo(2)
     }
 }
+
+private fun List<DepthChange>.countNumberOfDepthIncreases(): Int = count { it == INCREASED }
+
+private fun measure(report: List<Int>): List<DepthChange> = 
+    report.zipWithNext { previous, current -> if (current > previous) INCREASED else DECREASED }
 
 enum class DepthChange {
     INCREASED,
