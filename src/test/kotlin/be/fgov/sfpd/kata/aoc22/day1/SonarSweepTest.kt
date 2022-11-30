188da1e27255c04daa752f9f3ab58b2c6d98f25f
Jonas Van Hoeyweghen <jonas.VANHOEYWEGHEN@sfpd.fgov.be>
2022-11-30 10:23:12 +0100

    first implementation of measure depht changes

--- src/test/kotlin/be/fgov/sfpd/kata/aoc22/day1/SonarSweepTest.kt
+++ src/test/kotlin/be/fgov/sfpd/kata/aoc22/day1/SonarSweepTest.kt
@@ -1,24 +1,23 @@
-package be.fgov.sfpd.kata.aoc22.day1
-
-import be.fgov.sfpd.kata.aoc22.day1.Measurement.INCREASED
+import DepthChange.DECREASED
+import DepthChange.INCREASED
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 class SonarSweepTest {
-
     @Test
-    internal fun `given a list of two numbers check second one is larger`() {
-        val report = listOf(1, 2)
+    internal fun `given a sweep report, we measure the depth changes`() {
+        val report = listOf(1, 2, 3)
         val result = measure(report)
-        Assertions.assertThat(result).containsExactly(INCREASED)
+        Assertions.assertThat(result).containsExactly(INCREASED, INCREASED)
     }
 
-    private fun measure(sweep: List<Int>): List<Measurement> {
-       return listOf()
+    private fun measure(report: List<Int>): List<DepthChange> {
+        val depthMeasurements: List<Pair<Int, Int>> = report.zipWithNext()
+        return depthMeasurements.map { (previous, current) -> if (current > previous) INCREASED else DECREASED }
     }
 }
 
-enum class Measurement {
+enum class DepthChange {
     INCREASED,
     DECREASED
 }
