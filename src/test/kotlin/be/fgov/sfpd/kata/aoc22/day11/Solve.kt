233a9c70dcf3cada6b96ad9076b4449cce68c476
sch3lp <sch3lp@gmail.com>
2022-12-14 22:03:27 +0100

    Day 11 - Part 1 - Solved!

--- src/test/kotlin/be/fgov/sfpd/kata/aoc22/day11/Solve.kt
+++ src/test/kotlin/be/fgov/sfpd/kata/aoc22/day11/Solve.kt
@@ -55,39 +55,78 @@ class Solve {
         val input = monkeyBusiness {
             monkey(
                 id = 0,
-                startingItems = listOf(79, 98),
-                operation = { old -> old * 19 },
-                test = { it isDivisibleBy 23 },
-                onTrue = 2,
-                onFalse = 3,
+                startingItems = listOf(66, 71, 94),
+                operation = { old -> old * 5 },
+                test = { it isDivisibleBy 3 },
+                onTrue = 7,
+                onFalse = 4,
             )
+
             monkey(
                 id = 1,
-                startingItems = listOf(54, 65, 75, 74),
+                startingItems = listOf(70),
                 operation = { old -> old + 6 },
-                test = { it isDivisibleBy 19 },
-                onTrue = 2,
+                test = { it isDivisibleBy 17 },
+                onTrue = 3,
                 onFalse = 0,
             )
+
             monkey(
                 id = 2,
-                startingItems = listOf(79, 60, 97),
-                operation = { old -> old * old },
-                test = { it isDivisibleBy 13 },
-                onTrue = 1,
-                onFalse = 3,
+                startingItems = listOf(62, 68, 56, 65, 94, 78),
+                operation = { old -> old + 5 },
+                test = { it isDivisibleBy 2 },
+                onTrue = 3,
+                onFalse = 1,
             )
+
             monkey(
                 id = 3,
-                startingItems = listOf(74),
-                operation = { old -> old + 3 },
-                test = { it isDivisibleBy 17 },
-                onTrue = 0,
+                startingItems = listOf(89, 94, 94, 67),
+                operation = { old -> old + 2 },
+                test = { it isDivisibleBy 19 },
+                onTrue = 7,
+                onFalse = 0,
+            )
+
+            monkey(
+                id = 4,
+                startingItems = listOf(71, 61, 73, 65, 98, 98, 63),
+                operation = { old -> old * 7 },
+                test = { it isDivisibleBy 11 },
+                onTrue = 5,
+                onFalse = 6,
+            )
+
+            monkey(
+                id = 5,
+                startingItems = listOf(55, 62, 68, 61, 60),
+                operation = { old -> old + 7 },
+                test = { it isDivisibleBy 5 },
+                onTrue = 2,
                 onFalse = 1,
+            )
+
+            monkey(
+                id = 6,
+                startingItems = listOf(93, 91, 69, 64, 72, 89, 50, 71),
+                operation = { old -> old + 1 },
+                test = { it isDivisibleBy 13 },
+                onTrue = 5,
+                onFalse = 2,
+            )
+
+            monkey(
+                id = 7,
+                startingItems = listOf(76, 50),
+                operation = { old -> old * old },
+                test = { it isDivisibleBy 7 },
+                onTrue = 4,
+                onFalse = 6,
             )
         }
 
-        assertThat(solve1(input)).isEqualTo(11820)
+        assertThat(solve1(input)).isEqualTo(55944)
     }
 
     @Test
