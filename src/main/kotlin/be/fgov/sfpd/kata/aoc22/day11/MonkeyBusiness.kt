063acbbdce97421fe69c30c095b811dece5db9c7
sch3lp <sch3lp@gmail.com>
2022-12-14 22:51:59 +0100

    Day 11 - Part 2 - Not getting there with just changing Ints to Longs, but not exploding neither?

--- src/main/kotlin/be/fgov/sfpd/kata/aoc22/day11/MonkeyBusiness.kt
+++ src/main/kotlin/be/fgov/sfpd/kata/aoc22/day11/MonkeyBusiness.kt
@@ -3,25 +3,39 @@ package be.fgov.sfpd.kata.aoc22.day11
 import be.fgov.sfpd.kata.aoc22.Debugging.debug
 
 class MonkeyInspector(
-    private val inspectedItemsPerMonkey: MutableMap<MonkeyId, Int> = mutableMapOf()
+    private val inspectedItemsPerMonkey: MutableMap<MonkeyId, Long> = mutableMapOf()
 ) {
-    fun inspect(monkeyId: MonkeyId, inspectedItems: Int) {
+    fun inspect(monkeyId: MonkeyId, inspectedItems: Long) {
         val updatedInspectedItems = inspectedItemsPerMonkey[monkeyId]?.let { it + inspectedItems } ?: inspectedItems
         inspectedItemsPerMonkey += monkeyId to updatedInspectedItems
     }
 
-    fun top(top: Int): List<Int> = inspectedItemsPerMonkey.debug { "inspectedItems per monkey: $it" }.values.sorted().takeLast(top).reversed()
+    fun top(top: Int): List<Long> =
+        inspectedItemsPerMonkey.debug { "inspectedItems per monkey: $it" }.values.sorted().takeLast(top).reversed()
+
+    fun print(round: Int): String {
+        return "== After round $round ==\n" +
+                inspectedItemsPerMonkey.entries
+                    .joinToString("\n") { (monkey, items) -> "Monkey $monkey inspected items $items times." }
+    }
 }
 
-class MonkeyBusiness private constructor() {
+class MonkeyBusiness private constructor(
+    private val worryCompensator: Boolean
+) {
     private val monkeys: MutableList<Monkey> = mutableListOf()
 
     fun play(rounds: Int, monkeyInspector: MonkeyInspector) =
-        repeat(rounds) { round(monkeyInspector) }
+        repeat(rounds) { round ->
+            playRound(monkeyInspector)
+            if (round in listOf(1, 20) + (1..10).map { it * 1000 }) {
+                monkeyInspector.debug { it.print(round)+"\n" }
+            }
+        }
 
-    private fun round(monkeyInspector: MonkeyInspector) {
+    private fun playRound(monkeyInspector: MonkeyInspector) {
         monkeys.forEach {
-            val itemsThrownAtMonkeys: List<Pair<MonkeyId, Int>> = it.inspectAndThrowItems(monkeyInspector)
+            val itemsThrownAtMonkeys: List<Pair<MonkeyId, Long>> = it.inspectAndThrowItems(monkeyInspector)
             itemsThrownAtMonkeys.forEach { (monkeyId, item) ->
                 monkeys[monkeyId].catch(item)
             }
@@ -29,18 +43,28 @@ class MonkeyBusiness private constructor() {
     }
 
     companion object {
-        fun monkeyBusiness(setup: MonkeyBusiness.() -> Unit) =
-            MonkeyBusiness().apply { setup() }
+        fun monkeyBusiness(worryCompensator: Boolean = true, setup: MonkeyBusiness.() -> Unit) =
+            MonkeyBusiness(worryCompensator).apply { setup() }
 
         fun MonkeyBusiness.monkey(
             id: MonkeyId,
             startingItems: List<Int>,
-            operation: (Int) -> Int,
-            test: (Int) -> Boolean,
+            operation: (Long) -> Long,
+            test: (Long) -> Boolean,
             onTrue: MonkeyId,
             onFalse: MonkeyId,
         ) {
-            monkeys.add(Monkey(id, startingItems.toMutableList(), operation, test, onTrue, onFalse))
+            monkeys.add(
+                Monkey(
+                    id,
+                    startingItems.map { it.toLong() }.toMutableList(),
+                    operation,
+                    test,
+                    onTrue,
+                    onFalse,
+                    worryCompensator,
+                ).debug { "added Monkey ${it.id} with worryCompensator = $worryCompensator\n" }
+            )
         }
     }
 }
@@ -49,24 +73,26 @@ typealias MonkeyId = Int
 
 data class Monkey(
     val id: MonkeyId,
-    val items: MutableList<Int>,
-    val worryLevelOperation: (Int) -> Int,
-    val test: (Int) -> Boolean,
+    val items: MutableList<Long>,
+    val worryLevelOperation: (Long) -> Long,
+    val test: (Long) -> Boolean,
     val trueMonkey: MonkeyId,
     val falseMonkey: MonkeyId,
+    val worryCompensator: Boolean,
 ) {
-    fun inspectAndThrowItems(monkeyInspector: MonkeyInspector): List<Pair<MonkeyId, Int>> =
+
+    fun inspectAndThrowItems(monkeyInspector: MonkeyInspector): List<Pair<MonkeyId, Long>> =
         items.map { item ->
             monkeyInspector.inspect(id, 1)
-            val newWorryLevel = worryLevelOperation(item) / 3
+            val newWorryLevel = if (worryCompensator) worryLevelOperation(item) / 3 else worryLevelOperation(item)
             if (test(newWorryLevel)) newWorryLevel throwTo trueMonkey
             else newWorryLevel throwTo falseMonkey
         }.also { items.clear() }
 
-    fun catch(item: Int) = items.add(item)
+    fun catch(item: Long) = items.add(item)
 
-    private infix fun Int.throwTo(monkeyId: MonkeyId) =
+    private infix fun Long.throwTo(monkeyId: MonkeyId) =
         monkeyId to this
 }
 
-infix fun Int.isDivisibleBy(other: Int) = this % other == 0
+infix fun Long.isDivisibleBy(other: Int) = this % other == 0L
--- src/test/kotlin/be/fgov/sfpd/kata/aoc22/day11/Solve.kt
+++ src/test/kotlin/be/fgov/sfpd/kata/aoc22/day11/Solve.kt
@@ -1,6 +1,7 @@
 package be.fgov.sfpd.kata.aoc22.day11
 
 import be.fgov.sfpd.kata.aoc22.Debugging
+import be.fgov.sfpd.kata.aoc22.Debugging.withDebugging
 import be.fgov.sfpd.kata.aoc22.day11.MonkeyBusiness.Companion.monkey
 import be.fgov.sfpd.kata.aoc22.day11.MonkeyBusiness.Companion.monkeyBusiness
 import be.fgov.sfpd.kata.aoc22.readFile
@@ -11,142 +12,150 @@ class Solve {
 
     @Test
     fun `example input part 1`() {
-        val input = monkeyBusiness {
-            monkey(
-                id = 0,
-                startingItems = listOf(79, 98),
-                operation = { old -> old * 19 },
-                test = { it isDivisibleBy 23 },
-                onTrue = 2,
-                onFalse = 3,
-            )
-            monkey(
-                id = 1,
-                startingItems = listOf(54, 65, 75, 74),
-                operation = { old -> old + 6 },
-                test = { it isDivisibleBy 19 },
-                onTrue = 2,
-                onFalse = 0,
-            )
-            monkey(
-                id = 2,
-                startingItems = listOf(79, 60, 97),
-                operation = { old -> old * old },
-                test = { it isDivisibleBy 13 },
-                onTrue = 1,
-                onFalse = 3,
-            )
-            monkey(
-                id = 3,
-                startingItems = listOf(74),
-                operation = { old -> old + 3 },
-                test = { it isDivisibleBy 17 },
-                onTrue = 0,
-                onFalse = 1,
-            )
+        withDebugging {
+            assertThat(solve1(exampleInput)).isEqualTo(10605L)
         }
-        Debugging.enable()
-        assertThat(solve1(input)).isEqualTo(10605)
-        Debugging.disable()
     }
 
     @Test
     fun `actual input part 1`() {
-        val input = monkeyBusiness {
-            monkey(
-                id = 0,
-                startingItems = listOf(66, 71, 94),
-                operation = { old -> old * 5 },
-                test = { it isDivisibleBy 3 },
-                onTrue = 7,
-                onFalse = 4,
-            )
-
-            monkey(
-                id = 1,
-                startingItems = listOf(70),
-                operation = { old -> old + 6 },
-                test = { it isDivisibleBy 17 },
-                onTrue = 3,
-                onFalse = 0,
-            )
-
-            monkey(
-                id = 2,
-                startingItems = listOf(62, 68, 56, 65, 94, 78),
-                operation = { old -> old + 5 },
-                test = { it isDivisibleBy 2 },
-                onTrue = 3,
-                onFalse = 1,
-            )
-
-            monkey(
-                id = 3,
-                startingItems = listOf(89, 94, 94, 67),
-                operation = { old -> old + 2 },
-                test = { it isDivisibleBy 19 },
-                onTrue = 7,
-                onFalse = 0,
-            )
-
-            monkey(
-                id = 4,
-                startingItems = listOf(71, 61, 73, 65, 98, 98, 63),
-                operation = { old -> old * 7 },
-                test = { it isDivisibleBy 11 },
-                onTrue = 5,
-                onFalse = 6,
-            )
-
-            monkey(
-                id = 5,
-                startingItems = listOf(55, 62, 68, 61, 60),
-                operation = { old -> old + 7 },
-                test = { it isDivisibleBy 5 },
-                onTrue = 2,
-                onFalse = 1,
-            )
-
-            monkey(
-                id = 6,
-                startingItems = listOf(93, 91, 69, 64, 72, 89, 50, 71),
-                operation = { old -> old + 1 },
-                test = { it isDivisibleBy 13 },
-                onTrue = 5,
-                onFalse = 2,
-            )
-
-            monkey(
-                id = 7,
-                startingItems = listOf(76, 50),
-                operation = { old -> old * old },
-                test = { it isDivisibleBy 7 },
-                onTrue = 4,
-                onFalse = 6,
-            )
-        }
-
-        assertThat(solve1(input)).isEqualTo(55944)
+        assertThat(solve1(actualInput)).isEqualTo(55944L)
     }
 
     @Test
     fun `example input part 2`() {
-        val input = readFile("day11/exampleInput.txt")
-        assertThat(solve2(input)).isEqualTo(11)
+        withDebugging {
+            assertThat(solve2(exampleInput)).isEqualTo(2713310158L)
+        }
     }
 
     @Test
     fun `actual input part 2`() {
-        val input = readFile("day11/input.txt")
-        assertThat(solve2(input)).isEqualTo(11)
+        assertThat(solve2(actualInput)).isEqualTo(11)
     }
 
-    private fun solve1(monkeyBusiness: MonkeyBusiness): Int {
+    private fun solve1(input: MonkeyBusiness.() -> Unit): Long {
         val monkeyInspector = MonkeyInspector()
-        monkeyBusiness.play(20, monkeyInspector)
+        monkeyBusiness(setup = input).play(20, monkeyInspector)
         val (mostActiveMonkey, secondMostActiveMonkey) = monkeyInspector.top(2)
         return mostActiveMonkey * secondMostActiveMonkey
     }
 
-    private fun solve2(input: String): Int = TODO()
+    private fun solve2(input: MonkeyBusiness.() -> Unit): Long {
+        val monkeyInspector = MonkeyInspector()
+        monkeyBusiness(worryCompensator = false, setup = input).play(10_000, monkeyInspector)
+        val (mostActiveMonkey, secondMostActiveMonkey) = monkeyInspector.top(2)
+        return mostActiveMonkey * secondMostActiveMonkey
+    }
+
+    private val exampleInput: MonkeyBusiness.() -> Unit = {
+        monkey(
+            id = 0,
+            startingItems = listOf(79, 98),
+            operation = { old -> old * 19 },
+            test = { it isDivisibleBy 23 },
+            onTrue = 2,
+            onFalse = 3,
+        )
+        monkey(
+            id = 1,
+            startingItems = listOf(54, 65, 75, 74),
+            operation = { old -> old + 6 },
+            test = { it isDivisibleBy 19 },
+            onTrue = 2,
+            onFalse = 0,
+        )
+        monkey(
+            id = 2,
+            startingItems = listOf(79, 60, 97),
+            operation = { old -> old * old },
+            test = { it isDivisibleBy 13 },
+            onTrue = 1,
+            onFalse = 3,
+        )
+        monkey(
+            id = 3,
+            startingItems = listOf(74),
+            operation = { old -> old + 3 },
+            test = { it isDivisibleBy 17 },
+            onTrue = 0,
+            onFalse = 1,
+        )
+    }
+
+
+    private val actualInput: MonkeyBusiness.() -> Unit = {
+        monkey(
+            id = 0,
+            startingItems = listOf(66, 71, 94),
+            operation = { old -> old * 5 },
+            test = { it isDivisibleBy 3 },
+            onTrue = 7,
+            onFalse = 4,
+        )
+
+        monkey(
+            id = 1,
+            startingItems = listOf(70),
+            operation = { old -> old + 6 },
+            test = { it isDivisibleBy 17 },
+            onTrue = 3,
+            onFalse = 0,
+        )
+
+        monkey(
+            id = 2,
+            startingItems = listOf(62, 68, 56, 65, 94, 78),
+            operation = { old -> old + 5 },
+            test = { it isDivisibleBy 2 },
+            onTrue = 3,
+            onFalse = 1,
+        )
+
+        monkey(
+            id = 3,
+            startingItems = listOf(89, 94, 94, 67),
+            operation = { old -> old + 2 },
+            test = { it isDivisibleBy 19 },
+            onTrue = 7,
+            onFalse = 0,
+        )
+
+        monkey(
+            id = 4,
+            startingItems = listOf(71, 61, 73, 65, 98, 98, 63),
+            operation = { old -> old * 7 },
+            test = { it isDivisibleBy 11 },
+            onTrue = 5,
+            onFalse = 6,
+        )
+
+        monkey(
+            id = 5,
+            startingItems = listOf(55, 62, 68, 61, 60),
+            operation = { old -> old + 7 },
+            test = { it isDivisibleBy 5 },
+            onTrue = 2,
+            onFalse = 1,
+        )
+
+        monkey(
+            id = 6,
+            startingItems = listOf(93, 91, 69, 64, 72, 89, 50, 71),
+            operation = { old -> old + 1 },
+            test = { it isDivisibleBy 13 },
+            onTrue = 5,
+            onFalse = 2,
+        )
+
+        monkey(
+            id = 7,
+            startingItems = listOf(76, 50),
+            operation = { old -> old * old },
+            test = { it isDivisibleBy 7 },
+            onTrue = 4,
+            onFalse = 6,
+        )
+    }
+
 }
